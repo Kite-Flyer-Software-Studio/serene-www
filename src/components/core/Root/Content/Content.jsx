815db7f41db272c21b52885cf069e053e74ca051
reico <leereico@gmail.com>
2024-10-24 03:03:53 +0800

    feat: marquee below hero

--- src/components/core/Root/Content/Content.jsx
+++ src/components/core/Root/Content/Content.jsx
@@ -49,6 +49,24 @@ export default function Content({
 
   return (
     <>
+      {/* Marquee Section */}
+      <div className="page-section overflow-hidden pt-0 pb-0">
+        {/* Marquee Text Line */}
+        <div className="marquee marquee-style-2 no-rotate">
+          <div className="marquee-track marquee-animation-1">
+            <div>"Be True To Your Self"</div>
+            <div aria-hidden="true">"Friendship First"</div>
+            <div aria-hidden="true">"Safe Space, Open Hearts"</div>
+            <div aria-hidden="true">"Judgement Free"</div>
+            <div aria-hidden="true">"No Assholes"</div>
+            <div aria-hidden="true">"Open Up, Be Real"</div>
+            <div aria-hidden="true">"Engage, Don't Just Exist"</div>
+            <div aria-hidden="true">"Savouring More Than Just Food"</div>
+            <div aria-hidden="true">"Face-to-Face Connections"</div>
+          </div>
+        </div>
+        {/* End Marquee Text Line */}
+      </div>
       <section
         className={`page-section  scrollSpysection ${
           dark ? 'bg-dark-2 ' : 'bg-dark-1'
