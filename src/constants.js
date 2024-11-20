6c17faf397c182199a5f1a2de74c3379c3aac4ee
reico <leereico@gmail.com>
2024-11-20 16:37:37 +0800

    feat: udpate cta links to mighty

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,15 @@
 export const TYPEFORM_WAITLIST_EN_URL =
-  'https://serene-experience.typeform.com/sign-up'
+  'https://serene-experience.typeform.com/sign-up';
 
 export const TYPEFORM_WAITLIST_ZH_URL =
-  'https://serene-experience.typeform.com/to/tTZQOKvW'
+  'https://serene-experience.typeform.com/to/tTZQOKvW';
 
-export const LUMA_REGULAR_CALENDAR = 'https://lu.ma/serene-regular'
+export const MIGHTY_LANDING_URL = 'https://my-serene.mn.co/';
 
-export const GTM_ID = `GTM-PLF2BT79`
+export const LUMA_REGULAR_CALENDAR = 'https://lu.ma/serene-regular';
 
-export const META_PIXEL_ID = `2432729933589759`
+export const GTM_ID = `GTM-PLF2BT79`;
 
-export const LUMA_API_KEY = `secret-zJXDWoXdrwOhaC1k0eLG8Baac`
+export const META_PIXEL_ID = `2432729933589759`;
+
+export const LUMA_API_KEY = `secret-zJXDWoXdrwOhaC1k0eLG8Baac`;
--- src/hooks/useTypeformWaitlistUrl.js
+++ src/hooks/useTypeformWaitlistUrl.js
@@ -1,11 +1,5 @@
-import useLocale from '@/i18n/useLocale'
-import {
-  TYPEFORM_WAITLIST_EN_URL,
-  TYPEFORM_WAITLIST_ZH_URL,
-} from '@/constants'
+import { MIGHTY_LANDING_URL } from '@/constants';
 
 export default function useTypeformWaitlistUrl() {
-  const locale = useLocale()
-
-  return locale == 'en' ? TYPEFORM_WAITLIST_EN_URL : TYPEFORM_WAITLIST_ZH_URL
+  return MIGHTY_LANDING_URL;
 }
