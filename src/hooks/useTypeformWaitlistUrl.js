5677bbfcef7c7e6f9d2325a4e9a740f27a982ba4
reico <leereico@gmail.com>
2024-11-25 13:01:56 +0800

    feat: change back to typeform url for ctas

--- src/hooks/useTypeformWaitlistUrl.js
+++ src/hooks/useTypeformWaitlistUrl.js
@@ -1,5 +1,11 @@
-import { MIGHTY_LANDING_URL } from '@/constants';
+import useLocale from '@/i18n/useLocale';
+import {
+  TYPEFORM_WAITLIST_EN_URL,
+  TYPEFORM_WAITLIST_ZH_URL,
+  // MIGHTY_LANDING_URL,
+} from '@/constants';
 
 export default function useTypeformWaitlistUrl() {
-  return MIGHTY_LANDING_URL;
+  const locale = useLocale();
+  return locale == 'en' ? TYPEFORM_WAITLIST_EN_URL : TYPEFORM_WAITLIST_ZH_URL;
 }
